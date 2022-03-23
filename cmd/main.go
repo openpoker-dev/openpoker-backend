13b48da91bc3ed0f038532c26e0be41d988fafca
Xu Hao <jace.xh@gmail.com>
2022-03-23 18:25:24 +0800

    Feature/dockerize (#1)
    * test
    
    * change tag type
    
    * update
    
    * fix path
    
    * update
    
    * update

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+var (
+	htmlContent = []byte(`hello world`)
+	version     = "v0.0.1"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Header.Get("User-Agent"))
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(htmlContent)
+}
 
 func main() {
-	fmt.Println("hello world")
+	log.Println("running openpoker: " + version)
+
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
